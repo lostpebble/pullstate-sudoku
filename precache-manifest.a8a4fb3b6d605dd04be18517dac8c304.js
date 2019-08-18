73de820c05d8f8ed37c47a422891a12b8875cbcb
Paul Myburgh <paulmyburgh88@gmail.com>
2019-08-18 16:15:27 +0200

    Updates

---
+++ precache-manifest.a8a4fb3b6d605dd04be18517dac8c304.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e172e20a11df97443b9ed8d8c619ae9a",
+    "url": "/pullstate-sudoku/index.html"
+  },
+  {
+    "revision": "19965f3b105438a51aa8",
+    "url": "/pullstate-sudoku/static/css/main.d6b0db4f.chunk.css"
+  },
+  {
+    "revision": "09075975bd65661881fc",
+    "url": "/pullstate-sudoku/static/js/2.51ef9857.chunk.js"
+  },
+  {
+    "revision": "19965f3b105438a51aa8",
+    "url": "/pullstate-sudoku/static/js/main.be739057.chunk.js"
+  },
+  {
+    "revision": "cf72c145a377f1e1919d",
+    "url": "/pullstate-sudoku/static/js/runtime~main.aabd2685.js"
+  }
+]);
